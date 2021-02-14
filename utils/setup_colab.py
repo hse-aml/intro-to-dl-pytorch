ecb8e8a9dd49903132794fc936a9340363df9809
birshert <birshertalexey21@gmail.com>
2021-02-14 16:20:30 +0300

    upload week01 and utils

---
+++ utils/setup_colab.py
@@ -0,0 +1,14 @@
+import os
+
+
+def download_github_code(path):
+    filename = path.rsplit('/')[-1]
+    os.system('shred -u {}'.format(filename))
+    os.system('wget https://raw.githubusercontent.com/hse-aml/intro-to-dl-pytorch/main/{} -O {}'.format(path, filename))
+
+
+def setup_week01():
+    download_github_code('utils/grading.py')
+    download_github_code('week01/data/train.npy')
+    download_github_code('week01/data/target.npy')
+

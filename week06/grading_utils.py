bc173974ee3df7abae7727c7029f77600348c544
birshert <birshertalexey21@gmail.com>
2021-02-14 23:22:36 +0300

    week06 updates

---
+++ week06/grading_utils.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+def test_vocab(vocab):
+    return [
+        len(vocab),
+        len(np.unique(list(vocab.values()))),
+    ]
+
+
+def test_network(network):
+    return int(all(param.grad is not None for param in network.parameters()))
+
+
+def test_batch(batch):
+    return int(batch[0].shape[0])
